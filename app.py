ca6d5c932445838402c9904ddd6482ffc2ab9a0b
Rodrigo <rmoreirao@gmail.com>
2018-04-01 15:08:23 +0200

    ngular!

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import requests
 import operator
 import re
 import nltk
-from flask import Flask, render_template, request
+import json
+from rq import Queue
+from rq.job import Job
+from worker import conn
+from flask import Flask, render_template, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
-from rq import Queue
-from rq.job import Job
-from worker import conn
-from flask import jsonify
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -20,10 +21,10 @@ db = SQLAlchemy(app)
 
 q = Queue(connection=conn)
 
-from models import Result
+from models import *
+
 
 def count_and_save_words(url):
-    print("running method count_and_save_words!")
 
     errors = []
 
@@ -33,11 +34,10 @@ def count_and_save_words(url):
         errors.append(
             "Unable to get URL. Please make sure it's valid and try again."
         )
-        print(errors)
         return {"error": errors}
 
     # text processing
-    raw = BeautifulSoup(r.text).get_text()
+    raw = BeautifulSoup(r.text, 'html.parser').get_text()
     nltk.data.path.append('./nltk_data/')  # set the path
     tokens = nltk.word_tokenize(raw)
     text = nltk.Text(tokens)
@@ -63,30 +63,33 @@ def count_and_save_words(url):
         return result.id
     except:
         errors.append("Unable to add item to database.")
-        print(errors)
         return {"error": errors}
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    results = {}
-    if request.method == "POST":
-        # get url that the person has entered
-        url = request.form['url']
-        if 'http://' not in url[:7]:
-            url = 'http://' + url
-        job = q.enqueue_call(
-            func=count_and_save_words, args=(url,), result_ttl=5000
-        )
-        print(job.get_id())
+    return render_template('index.html')
 
-    return render_template('index.html', results=results)
+
+@app.route('/start', methods=['POST'])
+def get_counts():
+    # get url
+    data = json.loads(request.data.decode())
+    url = data["url"]
+    if 'http://' not in url[:7]:
+        url = 'http://' + url
+    # start job
+    job = q.enqueue_call(
+        func=count_and_save_words, args=(url,), result_ttl=5000
+    )
+    # return created job id
+    return job.get_id()
+
 
 @app.route("/results/<job_key>", methods=['GET'])
 def get_results(job_key):
 
     job = Job.fetch(job_key, connection=conn)
-
-    print(job)
 
     if job.is_finished:
         result = Result.query.filter_by(id=job.result).first()
@@ -98,6 +101,7 @@ def get_results(job_key):
         return jsonify(results)
     else:
         return "Nay!", 202
+
 
 if __name__ == '__main__':
     app.run()
